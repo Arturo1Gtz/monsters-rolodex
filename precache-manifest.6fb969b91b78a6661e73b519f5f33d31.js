ccab0939e4b5ca8ccd5b13ae3c30484362acf469
Arturo Gutierrez <arturo1gutierrez@outlook.com>
2020-05-08 15:32:39 -0500

    Updates

---
+++ precache-manifest.6fb969b91b78a6661e73b519f5f33d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a622f889a188353329770fc67a5f3e05",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ab9c38d26b3d3be614fe",
+    "url": "/monsters-rolodex/static/css/main.44a86120.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monsters-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9c38d26b3d3be614fe",
+    "url": "/monsters-rolodex/static/js/main.70b39eb8.chunk.js"
+  },
+  {
+    "revision": "df5d6ef2e44460cbab72",
+    "url": "/monsters-rolodex/static/js/runtime-main.51942013.js"
+  }
+]);
